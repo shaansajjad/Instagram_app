1830384b482255ffe0973d82a083e268ca17adf1
sajjad <shaankhan178@gmail.com>
2018-04-12 18:58:31 +0530

    testing slack

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406064234) do
+ActiveRecord::Schema.define(version: 20180410055744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180406064234) do
     t.string "first_name"
     t.string "last_name"
     t.date "birthday"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
